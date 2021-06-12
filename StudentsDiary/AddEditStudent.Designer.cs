d04365789ebc2ca4342b240c1e7fda940d410a57
adekeagle <2726949-adekeagle@users.noreply.gitlab.com>
2021-06-12 16:50:34 +0200

    Added checkboxes and their operation

--- StudentsDiary/AddEditStudent.Designer.cs
+++ StudentsDiary/AddEditStudent.Designer.cs
@@ -50,6 +50,7 @@ namespace StudentsDiary
             this.btnCancel = new System.Windows.Forms.Button();
             this.btnConfirm = new System.Windows.Forms.Button();
             this.cbIsAdditionalActivities = new System.Windows.Forms.CheckBox();
+            this.cboClassName = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // tbId
@@ -200,7 +201,7 @@ namespace StudentsDiary
             // 
             // btnCancel
             // 
-            this.btnCancel.Location = new System.Drawing.Point(114, 455);
+            this.btnCancel.Location = new System.Drawing.Point(118, 502);
             this.btnCancel.Name = "btnCancel";
             this.btnCancel.Size = new System.Drawing.Size(78, 29);
             this.btnCancel.TabIndex = 20;
@@ -210,7 +211,7 @@ namespace StudentsDiary
             // 
             // btnConfirm
             // 
-            this.btnConfirm.Location = new System.Drawing.Point(206, 454);
+            this.btnConfirm.Location = new System.Drawing.Point(210, 501);
             this.btnConfirm.Name = "btnConfirm";
             this.btnConfirm.Size = new System.Drawing.Size(78, 29);
             this.btnConfirm.TabIndex = 21;
@@ -221,18 +222,28 @@ namespace StudentsDiary
             // cbIsAdditionalActivities
             // 
             this.cbIsAdditionalActivities.AutoSize = true;
-            this.cbIsAdditionalActivities.Location = new System.Drawing.Point(105, 411);
+            this.cbIsAdditionalActivities.Location = new System.Drawing.Point(24, 461);
             this.cbIsAdditionalActivities.Name = "cbIsAdditionalActivities";
             this.cbIsAdditionalActivities.Size = new System.Drawing.Size(273, 17);
             this.cbIsAdditionalActivities.TabIndex = 22;
             this.cbIsAdditionalActivities.Text = "Czy uczeń bierze udział w dodatkowych zajęciach ?";
             this.cbIsAdditionalActivities.UseVisualStyleBackColor = true;
             // 
+            // cboClassName
+            // 
+            this.cboClassName.FormattingEnabled = true;
+            this.cboClassName.Location = new System.Drawing.Point(105, 416);
+            this.cboClassName.Name = "cboClassName";
+            this.cboClassName.Size = new System.Drawing.Size(183, 21);
+            this.cboClassName.TabIndex = 23;
+            this.cboClassName.SelectedIndexChanged += new System.EventHandler(this.cboClassName_SelectedIndexChanged);
+            // 
             // AddEditStudent
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(316, 512);
+            this.ClientSize = new System.Drawing.Size(316, 552);
+            this.Controls.Add(this.cboClassName);
             this.Controls.Add(this.cbIsAdditionalActivities);
             this.Controls.Add(this.btnConfirm);
             this.Controls.Add(this.btnCancel);
@@ -254,8 +265,8 @@ namespace StudentsDiary
             this.Controls.Add(this.tbFirstName);
             this.Controls.Add(this.lbId);
             this.Controls.Add(this.tbId);
-            this.MaximumSize = new System.Drawing.Size(332, 551);
-            this.MinimumSize = new System.Drawing.Size(332, 551);
+            this.MaximumSize = new System.Drawing.Size(332, 591);
+            this.MinimumSize = new System.Drawing.Size(332, 591);
             this.Name = "AddEditStudent";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Dodawanie ucznia";
@@ -284,9 +295,9 @@ namespace StudentsDiary
         private System.Windows.Forms.TextBox tbForeignLang;
         private System.Windows.Forms.RichTextBox rtbComments;
         private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button btnCancel;
         private System.Windows.Forms.Button btnConfirm;
         private System.Windows.Forms.CheckBox cbIsAdditionalActivities;
+        private System.Windows.Forms.ComboBox cboClassName;
     }
 }
